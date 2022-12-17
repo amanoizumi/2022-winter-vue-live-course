35afad7d09c146c4547539915b8361cf306eed21
amanoizumi <a0918233246@gmail.com>
2022-12-17 19:11:25 +0800

    update: week3

--- week3/admin.js
+++ week3/admin.js
@@ -1,5 +1,5 @@
-import 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 /*
 - lv 2
@@ -12,10 +12,30 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 const app = createApp({
   data() {
     return {
-      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
-      apiPath: 'i-fitness',
-      tempProduct: {},
+      apiUrl: "https://vue3-course-api.hexschool.io/v2",
+      apiPath: "i-fitness",
+      tempProduct: {
+        category: "",
+        content: "",
+        description: "",
+        imageUrl: "",
+        imagesUrl: [],
+        is_enabled: false,
+        num: 0,
+        origin_price: 0,
+        price: 0,
+        title: "",
+        unit: "",
+      },
+
+      isEdit: false,
+      uploadedImageUrl: "",
       products: [],
+
+      deleteProduct: {
+        id: "",
+        title: "",
+      },
     };
   },
   methods: {
@@ -30,7 +50,7 @@ const app = createApp({
         .catch((err) => {
           alert(err.response.data.message);
           // 出錯則跳回首頁
-          window.location = 'login.html';
+          window.location = "login.html";
         });
     },
     getProducts() {
@@ -39,7 +59,6 @@ const app = createApp({
         .get(url)
         .then((res) => {
           this.products = res.data.products;
-          console.log(this.products);
         })
         .catch((err) => {
           alert(err.response.data.message);
@@ -55,15 +74,17 @@ const app = createApp({
         },
       };
       const url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${productSend.id}`;
+
       axios
         .put(url, putData)
         .then((res) => {
           if (res.status === 200) {
-            alert('成功編輯啟用狀態！');
+            alert("成功編輯啟用狀態！");
             // 如果回傳 200，就自行改變資料。不重新用 getProducts 發請求
             this.products.forEach((product, idx) => {
               if (product.id === productSend.id) {
-                this.products[idx].is_enabled = !this.products[idx].is_enabled * 1;
+                this.products[idx].is_enabled =
+                  !this.products[idx].is_enabled * 1;
               }
             });
           }
@@ -72,21 +93,131 @@ const app = createApp({
           alert(err.response.data.message);
         });
     },
+    editProduct(product) {
+      this.isEdit = true;
+
+      this.tempProduct = { ...product };
+
+      if (!this.tempProduct.hasOwnProperty("imagesUrl")) {
+        this.tempProduct.imagesUrl = [];
+      }
+    },
+    addProduct() {
+      this.isEdit = false;
+      this.resetTempProduct();
+    },
+    confirmAddOrEdit() {
+      let url = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
+      let handlerStr = "post";
+
+      if (this.isEdit) {
+        handlerStr = "put";
+        url = `${url}/${this.tempProduct.id}`;
+      }
+
+      axios[handlerStr](url, { data: this.tempProduct })
+        .then((res) => {
+          this.resetTempProduct();
+          this.getProducts();
+          alert(`已成功${handlerStr === "put" ? "編輯" : "新增"}產品！`);
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+    deleteProductModal(product) {
+      this.deleteProduct.title = product.title;
+      this.deleteProduct.id = product.id;
+    },
+    confirmDeleteProduct() {
+      let url = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.deleteProduct.id}`;
+
+      axios
+        .delete(url)
+        .then((res) => {
+          this.getProducts();
+          alert("成功刪除商品！");
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        })
+        .finally(() => {
+          this.deleteProduct.title = "";
+          this.deleteProduct.id = "";
+        });
+    },
+    // 清空暫存
+    resetTempProduct() {
+      this.uploadedImageUrl = "";
+      this.tempProduct = {
+        category: "",
+        content: "",
+        description: "",
+        imageUrl: "",
+        imagesUrl: [],
+        is_enabled: false,
+        num: 0,
+        origin_price: 0,
+        price: 0,
+        title: "",
+        unit: "",
+      };
+    },
+
+    // 上傳圖片
+    upload() {
+      const file = this.$refs.uploadImage.files[0];
+
+      const formData = new FormData();
+      formData.append("file-to-upload", file);
+
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/upload`;
+
+      axios
+        .post(url, formData)
+        .then((res) => {
+          this.uploadedImageUrl = res.data.imageUrl;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+    // 新增、替換主要圖片
+    addMainImage() {
+      this.tempProduct.imageUrl = this.uploadedImageUrl;
+      this.uploadedImageUrl = "";
+    },
+
+    // 新增到其他圖片
+    addImages() {
+      this.tempProduct.imagesUrl.push(this.uploadedImageUrl);
+      this.uploadedImageUrl = "";
+    },
+
+    // 刪除圖片
+    deleteImage(idx) {
+      this.tempProduct.imagesUrl.splice(idx, 1);
+    },
   },
+
   mounted() {
     // 參考範例程式碼
     // 取出 cookies 裡的 token
-    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    );
 
     // 設定 Authorization
     axios.defaults.headers.common.Authorization = token;
 
     this.checkLogin();
 
+    // throttle 避免連續發送請求
     // trailing: false
     // 觸發後，冷卻時間過後也不會再調用
     this.editEnabled = _.throttle(this.editEnabled, 2000, { trailing: false });
   },
 });
 
-app.mount('#app');
+app.mount("#app");
--- week3/login.js
+++ week3/login.js
@@ -1,12 +1,11 @@
-import 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js';
-
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+import "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 const app = createApp({
   data() {
     return {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       loginBtnDisabled: false,
     };
   },
@@ -14,7 +13,7 @@ const app = createApp({
     login() {
       this.loginBtnDisabled = true;
 
-      const url = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
+      const url = "https://vue3-course-api.hexschool.io/v2/admin/signin";
 
       const user = {
         username: this.email,
@@ -30,8 +29,10 @@ const app = createApp({
 
           // 寫入 cookie token
           // expires 設置有效時間
-          document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/`;
-          window.location = 'products.html';
+          document.cookie = `hexToken=${token};expires=${new Date(
+            expired
+          )}; path=/`;
+          window.location = "products.html";
         })
         .catch((err) => {
           alert(err.response.data.message);
@@ -43,4 +44,4 @@ const app = createApp({
   },
 });
 
-app.mount('#app');
+app.mount("#app");
